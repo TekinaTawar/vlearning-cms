9dc4565b0bb73776a783f9a0fb3dac660c41a5c6
TekinaTawar <astroaniket1@gmail.com>
2022-01-31 14:37:02 +0530

    added some entitites but not right detials

---
+++ src/api/video-lecture/controllers/video-lecture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  video-lecture controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::video-lecture.video-lecture');
---
+++ src/api/video-lecture/routes/video-lecture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * video-lecture router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::video-lecture.video-lecture');
